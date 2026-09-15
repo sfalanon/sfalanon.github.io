293033ff9a8d7e93e9dad7de40f7555504d87e3e
sfalanon <s.gitsfalanon@k5pta.com>
2026-09-15 11:26:41 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+
+const MYAT = '@';
+const MYNEXT1 = 'i';
+const MYPD='.';
+const MYNEXT2='c';
+const zz=String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYPD + 'webmaster' + MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
+
+  
+// Update the link attributes when the DOM loads
+const link = document.getElementById("email-link");
+link.href = "ma" + MYNEXT1 + "lto:" + zz;
+// link.innerText = fullEmail; // Optional: Changes 'Contact Us' to the email text
+
+
+
